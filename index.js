929873f8817e5b36dfb487275fc35e2e4acf2b59
Nadya2998 <Nadya2998@users.noreply.github.com>
2021-11-25 12:44:28 +0000

    Deploying to gh-pages from @ Nadya2998/Nadya29982@648b4095db7dbaba890cbf72de9c961bd5898888 🚀

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 "use strict"
+
 let product = "Дверь";
-let prise = "1500000";
-let c = `${product}, цена ${prise}`;
+let prise = "1000";
+let c = `${product}, цена ${prise} рублей`;
 alert(c);
+
+function number(min, max) {
+  if (min >= max) {
+  // eslint-disable-next-line
+  console.log('ошибочка вышла');
+  } else {
+  // eslint-disable-next-line
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+}
+number(5, 15);
